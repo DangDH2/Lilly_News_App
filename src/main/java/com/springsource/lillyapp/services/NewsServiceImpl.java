79343583fbbcad7cfa925012e5e265e735250e6a
ダン・ホー・ダン <e9900141@APCC649D.osaka.agrex.co.jp>
2017-03-27 11:30:00 +0900

    ss

--- src/main/java/com/springsource/lillyapp/services/NewsServiceImpl.java
+++ src/main/java/com/springsource/lillyapp/services/NewsServiceImpl.java
@@ -3,6 +3,7 @@ package com.springsource.lillyapp.services;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.springsource.lillyapp.dao.NewsDAO;
 import com.springsource.lillyapp.domain.News;
@@ -13,29 +14,34 @@ public class NewsServiceImpl implements NewsService {
 	private NewsDAO newsDAO;
 
 	@Override
+	@Transactional
 	public void addNews(News p) {
 		// TODO Auto-generated method stub
 
 	}
 
 	@Override
+	@Transactional
 	public void updateNews(News p) {
 		// TODO Auto-generated method stub
 
 	}
 
 	@Override
+	@Transactional
 	public List<News> listNewsByType(String type) {
 		return newsDAO.listNews(Integer.parseInt(type));
 	}
 
 	@Override
+	@Transactional
 	public News getNewsById(String id) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
+	@Transactional
 	public void removeNews(String id) {
 		// TODO Auto-generated method stub
 
