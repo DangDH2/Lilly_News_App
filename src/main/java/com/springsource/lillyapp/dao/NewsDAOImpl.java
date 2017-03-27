af2a1f3c0de5c154660a530367de91ef292017bc
ダン・ホー・ダン <e9900141@APCC649D.osaka.agrex.co.jp>
2017-03-27 14:20:12 +0900

    sâ

--- src/main/java/com/springsource/lillyapp/dao/NewsDAOImpl.java
+++ src/main/java/com/springsource/lillyapp/dao/NewsDAOImpl.java
@@ -1,11 +1,10 @@
 package com.springsource.lillyapp.dao;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.PersistenceUnit;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 
 import org.springframework.stereotype.Repository;
 
@@ -14,48 +13,34 @@ import com.springsource.lillyapp.domain.News;
 @Repository
 public class NewsDAOImpl implements NewsDAO {
 
-	private EntityManagerFactory emf;
-
-	@PersistenceUnit
-	public void setEntityManagerFactory(EntityManagerFactory emf) {
-		this.emf = emf;
-	}
+	@PersistenceContext
+	private EntityManager em;
 
 	@Override
 	public void addNews(News n) {
-		EntityManager em = this.emf.createEntityManager();
 		em.persist(n);
 
 	}
 
 	@Override
 	public void updateNews(News n) {
-		EntityManager em = this.emf.createEntityManager();
 
 	}
 
 	@Override
 	public List<News> listNews(int type) {
-		// Session session = this.sessionFactory.getCurrentSession();
-		// List<News> newsList = session.createQuery("from Account").list();
-		// return newsList;
-		List<News> newsList = new ArrayList<News>();
-		News n = new News();
-		n.setId("Id 1");
-		n.setTitle("Title 1");
-		newsList.add(n);
-		return newsList;
+		Query query = em.createQuery("from Product as p where p.category = :category");
+		// query.setParameter("category", category);
+		return query.getResultList();
 	}
 
 	@Override
 	public News getNewsId(String id) {
-		EntityManager em = this.emf.createEntityManager();
 		return new News();
 	}
 
 	@Override
 	public void removeNews(String id) {
-		EntityManager em = this.emf.createEntityManager();
 
 	}
 
